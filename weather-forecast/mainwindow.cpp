97885f762e344d4a309bed6e9738b573d9304a2e
Knock man <2380068646@qq.com>
2024-07-03 22:07:26 +0800

    完成UI设计

--- weather-forecast/mainwindow.cpp
+++ weather-forecast/mainwindow.cpp
@@ -9,6 +9,12 @@ MainWindow::MainWindow(QWidget *parent)
     //设置窗口属性为没有边框的窗口 没有标题栏
     setWindowFlag(Qt::FramelessWindowHint);
     setFixedSize(width(),height());//设置固定窗口大小
+    exitMenu = new QMenu(this);//创建一个菜单
+    exitAct = new QAction;//创建一个选项
+    //exitAct->setText(u8"退出");
+    exitAct->setIcon(QIcon(":/weatherIco/close.ico"));
+    exitMenu->addAction((exitAct));//选项添加进菜单
+    connect(exitAct,SIGNAL(triggered(bool)),this,SLOT(slo_exitApp()));//点击选项关闭窗口
 }
 
 MainWindow::~MainWindow()
@@ -16,3 +22,26 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+    exitMenu->exec(QCursor::pos());
+    event->accept();
+}
+
+void MainWindow::slo_exitApp()
+{
+    qApp->exit(0);
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    //新鼠标位置-原鼠标和控件的距离=新控件位置
+    move(event->globalPos()-mPos);
+}
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+
+    //鼠标的坐标-控件的坐标
+    mPos = event->globalPos()-this->pos();
+}
--- weather-forecast/mainwindow.h
+++ weather-forecast/mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QMenu>
+#include <QMouseEvent>
+#include <QAction>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +17,16 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void contextMenuEvent(QContextMenuEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+private slots:
+    void slo_exitApp();
 private:
     Ui::MainWindow *ui;
+    QMenu *exitMenu;//右键退出的菜单
+    QAction *exitAct;//退出的行为
+    QPoint mPos;//鼠标和控件的距离
 };
 #endif // MAINWINDOW_H
